461b905e5cd6638ad84c4e0227a3fb4328cb42a0
setvect <setvect@naver.com>
2020-09-06 17:17:43 +0900

    소크 코드 정리

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req: any, res: any, next: any) {
   res.render('index', { title: 'Express' });
 });
 
