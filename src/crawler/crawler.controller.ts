ef6526f121b05fb42f055224b7e11d3ed5354c67
setvect <setvect@naver.com>
2021-01-21 22:58:22 +0900

    axios 예제

--- src/crawler/crawler.controller.ts
+++ src/crawler/crawler.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from "@nestjs/common";
 import { CrawlerService } from "./crawler.service";
+import axios from "axios";
+import { AxiosResponse } from "axios";
 
 @Controller("crawl")
 export class CrawlerController {
   constructor(private readonly crawlService: CrawlerService) {}
 
   @Get()
-  getAll(): string {
-    return "abc1111222222";
+  async getAll(): Promise<string> {
+    const aaa: AxiosResponse = await axios({
+      method: "get",
+      url: "https://finance.naver.com/item/sise_day.nhn?code=005930&page=1",
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36",
+      },
+    });
+
+    console.log("aaa 1111:>> ", aaa);
+
+    return "11222";
   }
 }
