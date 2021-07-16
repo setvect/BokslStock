c53941770663fdca942b2d8c02c427fb89c6a51e
setvect <setvect@naver.com>
2021-07-16 20:52:03 +0900

    시각총액 수집

--- src/util/common-util.ts
+++ src/util/common-util.ts
@@ -22,4 +22,24 @@ export default class CommonUtil {
     }
     return instance;
   }
+
+  /**
+   * 정규표현식에서 사용하는 특수문자를 escape 처리함
+   * @param str
+   */
+  static escapeRegExp(str: string) {
+    return str.replace(/[\\-\\[\]\\/\\{\\}\\(\\)\\*\\+\\?\\.\\\\^\\$\\|]/g, "\\$&");
+  }
+
+  /**
+   * 문장 전체 치환
+   * @param text
+   * @param org
+   * @param dest
+   */
+  static replaceAll(text: string, org: string, dest: string): string {
+    const escape = CommonUtil.escapeRegExp(org);
+    const re = new RegExp(escape, "g");
+    return text.replace(re, dest);
+  }
 }
